087817e7f4f5b60a1e62bd897a26501a19183c56
marceloansantos <136930414+marceloansantos@users.noreply.github.com>
2024-03-26 19:26:47 -0400

    commit

---
+++ app/Http/Controllers/HelloWorldController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HelloWorldController extends Controller
+{
+    public function hello($name, Request $request)
+    {
+        return response()->json([
+            'oi' => "Hello World {$name}",
+            'tchau' => 'aquilo'
+        ]);
+    }
+}
